e3a60ac0f1b7fe5232967bb72ce4948390b6fa2a
darrix <dmhendricks@gmail.com>
2020-06-20 16:41:02 -0400

    modifed for random change

--- src/App.fs
+++ src/App.fs
@@ -8,12 +8,15 @@ let countViewer = document.getElementById "countViewer"
 
 let mutable currentCount = 0
 
+let rnd = System.Random()
+
 // add event handlers
-increase.onclick <- fun ev -> currentCount <- currentCount + 1
+increase.onclick <- fun ev -> currentCount <- currentCount + rnd.Next(5, 10)
                               countViewer.innerText <- sprintf "Count is %d" currentCount
 
-decrease.onclick <- fun ev -> currentCount <- currentCount - 1
+decrease.onclick <- fun ev -> currentCount <- currentCount - rnd.Next(5, 10)
                               countViewer.innerText <- sprintf "Count is %d" currentCount 
+
 
 // initializing the countViewer to start off
 countViewer.innerText <- sprintf "Count is %d" currentCount
