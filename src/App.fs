f2d5765226183cf208f32bffc1ed0fedd37166f5
darrix <dmhendricks@gmail.com>
2020-06-20 21:25:34 -0400

    async updates

--- src/App.fs
+++ src/App.fs
@@ -5,10 +5,19 @@ open Browser.Dom
 let increase = document.getElementById "increase"
 let decrease = document.getElementById "decrease"
 let countViewer = document.getElementById "countViewer"
+let increaseDelayed = document.getElementById "increaseDelayed"
+
+let runAfter ms callback =
+    async {
+        do! Async.Sleep ms 
+        do callback()
+    }
+    |> Async.StartImmediate
 
 let mutable currentCount = 0
 
 let rnd = System.Random()
+
 
 // add event handlers
 increase.onclick <- fun ev -> currentCount <- currentCount + rnd.Next(5, 10)
@@ -16,6 +25,14 @@ increase.onclick <- fun ev -> currentCount <- currentCount + rnd.Next(5, 10)
 
 decrease.onclick <- fun ev -> currentCount <- currentCount - rnd.Next(5, 10)
                               countViewer.innerText <- sprintf "Count is %d" currentCount 
+
+increaseDelayed.onclick <- fun _ ->
+        runAfter 1000    
+            ( 
+                fun () -> 
+                    currentCount <- currentCount + rnd.Next(5, 10)
+                    countViewer.innerText <- sprintf "Count is %d" currentCount
+            )
 
 
 // initializing the countViewer to start off
